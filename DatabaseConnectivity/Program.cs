9d5a32e73f573652769a4d7ab45b7a0a2595fdef
thedaamanda <thedaamandaa@gmail.com>
2023-04-01 01:16:42 +0700

    initial commit

---
+++ DatabaseConnectivity/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace DatabaseConnectivity
+{
+    public class Program
+    {
+        private static SqlConnection connection;
+        private static string connectionString = "Server=localhost; Database=db_hr_dts; User Id=sa; Password=Passw@rd; Trusted_Connection=False; MultipleActiveResultSets=true";
+
+        public static void Main(string[] args)
+        {
+            connection = new SqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connection Open!");
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Connection Failed : " + e);
+            }
+        }
+    }
+}
