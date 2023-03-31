fccf55a2d12d9fc0a5e42307114cf55f4ae7d6ff
thedaamanda <thedaamandaa@gmail.com>
2023-04-01 02:03:08 +0700

    feat: add insert region

--- DatabaseConnectivity/Program.cs
+++ DatabaseConnectivity/Program.cs
@@ -11,6 +11,7 @@ namespace DatabaseConnectivity
         public static void Main(string[] args)
         {
             GetAllRegion();
+            //InsertRegion("Region 2");
         }
 
         // method untuk menampilkan semua data region
@@ -45,6 +46,46 @@ namespace DatabaseConnectivity
 
             // menutup koneksi
             dbConn.CloseConnection();
+        }
+
+        // method untuk menambahkan data region
+        public static void InsertRegion(string name)
+        {
+            // membuka koneksi
+            dbConn.OpenConnection();
+
+            // membuat begin transaction
+            SqlTransaction transaction = dbConn.GetConnection().BeginTransaction();
+            try {
+                // membuat sql command
+                SqlCommand cmd = new SqlCommand("INSERT INTO region (name) VALUES (@name)", dbConn.GetConnection(), transaction);
+
+                // menambahkan parameter
+                cmd.Parameters.AddWithValue("@name", name);
+
+                // mengeksekusi sql command
+                int result = cmd.ExecuteNonQuery();
+
+                // menampilkan hasil eksekusi
+                if (result > 0)
+                {
+                    Console.WriteLine("Data berhasil ditambahkan");
+                }
+                else
+                {
+                    Console.WriteLine("Data gagal ditambahkan");
+                }
+
+                // commit transaction
+                transaction.Commit();
+            } catch (Exception ex) {
+                // rollback transaction
+                transaction.Rollback();
+                Console.WriteLine("Error: " + ex.Message);
+            } finally {
+                // menutup koneksi
+                dbConn.CloseConnection();
+            }
         }
     }
 }
